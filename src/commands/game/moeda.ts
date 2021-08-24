b8295b9cdb65e5b34408615afd19c821cf9b49ce
Ádisson Téjo <adissonmprtejo@gmail.com>
2021-08-24 20:44:00 -0300

    New command 'moeda'

---
+++ src/commands/game/moeda.ts
@@ -0,0 +1,13 @@
+import { CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder } from '@discordjs/builders';
+
+const command = {
+  data: new SlashCommandBuilder().setName('moeda').setDescription('Jogue cara ou coroa'),
+  async execute(interaction: CommandInteraction) {
+    const result = Math.floor(Math.random() * 2);
+
+    interaction.reply(`Você tirou ${result ? 'cara' : 'coroa'}`);
+  },
+};
+
+export default command;
