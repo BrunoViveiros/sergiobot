93d446e201e4be0ad463cfdc1b0adb54617ecefb
Petrus Bento <petrus@intellimize.com>
2021-08-27 16:47:58 -0300

    Update frase command

--- src/commands/image/frase.ts
+++ src/commands/image/frase.ts
@@ -1,7 +1,6 @@
-import { CommandInteraction, MessageAttachment, BufferResolvable } from 'discord.js';
+import { CommandInteraction, MessageAttachment } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { Canvas, resolveImage } from 'canvas-constructor/skia'; 
-import fetch from 'node-fetch'
+import { Canvas, resolveImage } from 'canvas-constructor/skia';
 
 const backgrounds = [
   'https://i.ibb.co/Wsdcd2x/berti.png',
@@ -12,21 +11,34 @@ const backgrounds = [
   'https://i.ibb.co/yYfCNDJ/dilma.png',
   'https://i.ibb.co/CWtnVvt/holt.png',
   'https://i.ibb.co/0jfQbxp/gilbala.png',
-]
+];
 
 const command = {
-data: new SlashCommandBuilder().setName('frase').setDescription('Crie uma frase famosa'),
+  data: new SlashCommandBuilder()
+    .setName('frase')
+    .setDescription('Crie uma frase famosa')
+    .addStringOption((option) =>
+      option
+        .setName('quote')
+        .setDescription('Escreva a frase')
+        .setRequired(true),
+    ),
   async execute(interaction: CommandInteraction) {
     const index = Math.floor(Math.random() * backgrounds.length);
-    // const bg = await fetch(backgrounds[index]);
-    const bg = await resolveImage(backgrounds[index])
-    
-    const img = new Canvas(600, 315).printImage(bg, 0, 0, 600, 315);
+    const bg = await resolveImage(backgrounds[index]);
+
+    const quote = interaction.options.getString('quote');
+
+    const img = new Canvas(600, 315)
+      .printImage(bg, 0, 0, 600, 315)
+      .setTextFont('24px')
+      .setColor('#fff')
+      .printWrappedText(`— ${quote}`, 30, 80, 300);
 
     const imgBuffer = await img.toBuffer('png');
-    const attachment  = new MessageAttachment(imgBuffer, 'frase.png');
+    const attachment = new MessageAttachment(imgBuffer, 'frase.png');
 
-    await interaction.reply({files: [attachment]});
+    await interaction.reply({ files: [attachment] });
   },
 };
 
