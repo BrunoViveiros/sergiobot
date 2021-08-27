deba4679bb6fb95e861572fba80dfc502c2a25c5
Raquel Patrício <raquel.moraes@thinkbox.com.br>
2021-08-27 11:50:28 -0300

    command frase, command detector

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,14 +1,10 @@
-import { CommandInteraction } from 'discord.js';
+import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import CreateGuildService from '../../services/createGuildService';
-
 
 const command = {
-  data: new SlashCommandBuilder().setName('ping').setDescription('Replies with pong'),
+data: new SlashCommandBuilder().setName('ping').setDescription('Responde com pong'),
   async execute(interaction: CommandInteraction) {
-    await interaction.reply(':ping_pong: Pong!');
-    const createGuild = new CreateGuildService()
-    createGuild.execute({name:'inferno'})
+    await interaction.reply({content: ':ping_pong: Pong!'});
   },
 };
 
