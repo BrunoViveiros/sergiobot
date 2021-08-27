be9d73e3cd4caddf2372f54b681b99b72d0bb5f7
Petrus Bento <petrus@intellimize.com>
2021-08-27 16:47:51 -0300

    Change ping command description

--- src/commands/utils/ping.ts
+++ src/commands/utils/ping.ts
@@ -1,10 +1,10 @@
-import { CommandInteraction, MessageActionRow, MessageButton } from 'discord.js';
+import { CommandInteraction } from 'discord.js';
 import { SlashCommandBuilder } from '@discordjs/builders';
 
 const command = {
   data: new SlashCommandBuilder()
     .setName('ping')
-    .setDescription('Replies with pong'),
+    .setDescription('Responde com pong'),
   async execute(interaction: CommandInteraction) {
     await interaction.reply(':ping_pong: Pong!');
   },
